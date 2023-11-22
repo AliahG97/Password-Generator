9689568e731d7225e63f4d95348f37c715c9fff6
AliahG97 <143744575+AliahG97@users.noreply.github.com>
2023-11-22 18:21:57 -0500

    added all rewuirements, just needs fine tuning

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -10,23 +10,47 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  var passwordLength = parseInT(prompt("Enter password length(betwee8 and 128):"));
-   if (IntersectionObserverEntry(passwordLength)  ||passwordLength < 8 || passwordLenth> 128) {
-    alert("please enter a valid password legnth between 8 and 128. 128.");
-    return"";
+    var passwordLength = parseInt(prompt("Enter password length(betwee8 and 128):"));
+    if (isNaN(passwordLength)  ||passwordLength < 8 || passwordLength> 128) {
+        alert("please enter a valid password legnth between 8 and 128. 128.");
+        return"";
+        
+    }
+    // selecting onoe of each Char type//
+    var includeLowercase = confirm("Include lowercase charachters?");
+    var includeUppercase = confirm("Include Uppercase charachters?");
+    var includeNumbers = confirm("Include numbers?");
+    var includeSpecialChars = confirm("Include special charachters?");
+    
+    if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecialChars) {
+        alert("please select at least one character type.");
+        return "";
+    }
+    // options of character to use//
+    var includeLowercase = "abcdefghijklmnopqrstuvwxyz";
+    var includeUppercase = "ABCDEFGHIJKLMNOPQRSTUVQXYZ";
+    var includeNumbers = "0123456789?";
+    var includeSpecialChars = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+    
+    var availableChars = "";
+    if (includeLowercase) availableChars += lowercaseChars;
+    var (includeUppercase) availableChars += UppercaseChars;
+  var (includeNumbers) availableChars += numberChars;
+  var (includeSpecialChars) availableChars += specialChars;
+  
+  vargeneratedPassword = "";
+  for (var i =  0; i < passwordLength; i++) {
+      var randomIndex = Math.floor(math.random() * availableChars.lenth);
+      generatedpassword += avaialablechars.charAt(randomIndex);
+    }
+    return genratedPassword;
+}
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = Password;
+}
 
-  }
-
-  var includeLowercase = confirm("Include lowercase charachters?");
-  var includeUppercase = confirm("Include Uppercase charachters?");
-  var includeNumbers = confirm("Include numbers?");
-  var includeSpecialChars = confirm("Include special charachters?");
-
-  if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecialChars) {
-    alert("please select at least one character type.");
-    return "";
-  }
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
